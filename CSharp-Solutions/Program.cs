6875b10e4d84a41ea6ab28a808c33f0508aa02f9
taleh.karimov <taleh.karimov@smartsolutions.az>
2025-03-25 15:17:37 +0400

    Add floyd solution for find duplicate number algorithm

---
+++ CSharp-Solutions/Program.cs
@@ -0,0 +1,9 @@
+﻿using CSharp_Solutions.Solutions;
+
+var solution = new FindDuplicateNumber();
+
+var arr = new int[] { 1, 3, 4, 2, 2 };
+
+
+
+Console.WriteLine(solution.FloydSolution(arr));
---
+++ CSharp-Solutions/Solutions/FindDuplicateNumber.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharp_Solutions.Solutions;
+
+internal class FindDuplicateNumber
+{
+    public int FloydSolution(int[] nums)
+    {
+        int slowPointer = nums[0];
+        int fastPointer = nums[0];
+
+        // Step 1: Detect cycle
+        do
+        {
+            slowPointer = nums[slowPointer]; // Move 1 step
+            fastPointer = nums[nums[fastPointer]]; // Move 2 steps
+        } while (slowPointer != fastPointer);
+
+        // Step 2: Find the entrance to the cycle
+        slowPointer = nums[0];
+        while (slowPointer != fastPointer)
+        {
+            slowPointer = nums[slowPointer];
+            fastPointer = nums[fastPointer];
+        };
+
+        return slowPointer;
+    }
+}
